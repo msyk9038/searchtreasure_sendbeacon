4cdfaecef15bb17c824f03be07ece11951ed7852
msyk9038 <41169937+msyk9038@users.noreply.github.com>
2021-12-15 09:08:24 +0900

    動作チェックのため，LEDを点滅させる．また，紛失時に備え，管理機からの信号に対して，音を出して応える．

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "bell") {
+        soundExpression.giggle.playUntilDone()
+    }
+})
 radio.setGroup(1)
 basic.forever(function () {
     radio.sendString("1")
+    basic.pause(200)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.clearScreen()
     basic.pause(200)
 })
