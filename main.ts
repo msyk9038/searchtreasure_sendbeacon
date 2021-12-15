e7f3370743c912437ed47d64af5a023b04bdfeb0
msyk9038 <41169937+msyk9038@users.noreply.github.com>
2021-12-15 09:28:34 +0900

    音がランダムで変わるようにした

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "bell") {
-        soundExpression.giggle.playUntilDone()
+        音の選択番号 = randint(0, 2)
+        if (音の選択番号 == 0) {
+            soundExpression.hello.playUntilDone()
+        } else if (音の選択番号 == 1) {
+            soundExpression.yawn.playUntilDone()
+        } else {
+            soundExpression.happy.playUntilDone()
+        }
     }
 })
+let 音の選択番号 = 0
 radio.setGroup(1)
 basic.forever(function () {
     radio.sendString("1")
